d9479bab12f323a5077c5ee0f3d3d9d1df3de425
eons <eons@eons.dev>
2023-02-12 13:01:23 -0800

    change func decl style

--- bash/git.func.bash
+++ bash/git.func.bash
@@ -1,20 +1,20 @@
-clone() {
+function clone {
     git clone https://github.com/${PWD##*/}/$1
 }
 
-commit() {
+function commit {
     git add .
     git commit -S -m "$1"
     git push
 }
 
-release() { 
+function release { 
     ver=$(gh release list | head -1 | awk '{print $1}' | awk -F. '{print $1"."$2"."$3+1}')
     gh release create $ver -t $ver --generate-notes
 }
 
 
-clone-org() {
+function clone-org {
     gh repo list $1 --limit 1000 | while read -r repo _; do
         gh repo clone "$repo" "$repo"
     done
